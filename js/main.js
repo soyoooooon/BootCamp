9d50885d69136ff311b7dfd65cc2497680b9244b
Jeffrey Helder <jeffrey.d.helder@gmail.com>
2019-09-18 17:12:39 -0400

    worked on CSS functionality, linked NAV/Buttons, initialized js file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(() => {
+	console.log('link successful');
+
+	let homeButton = document.querySelector('#home-btn'),
+	teamButton = document.querySelector('#home-btn'),
+	workButton = document.querySelector('#home-btn'),
+	contactButton = document.querySelector('#home-btn');
+
+})();
