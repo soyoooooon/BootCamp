fc0f558f746bb578672ce8f476913431cb467a9c
Jeffrey Helder <jeffrey.d.helder@gmail.com>
2019-09-18 20:26:45 -0400

    burger menu, offSet nav class

--- js/main.js
+++ js/main.js
@@ -1,9 +1,51 @@
 (() => {
 	console.log('link successful');
 
-	let homeButton = document.querySelector('#home-btn'),
-	teamButton = document.querySelector('#home-btn'),
-	workButton = document.querySelector('#home-btn'),
-	contactButton = document.querySelector('#home-btn');
+	let
+	burger = document.querySelector('#burger'),
+	burgerContent = document.querySelector('#mainNav'),
+	navFiller = document.querySelector('#mobileNavFiller'),
+	homeButton = document.querySelector('#home-nav'),
+	teamButton = document.querySelector('#team-nav'),
+	workButton = document.querySelector('#work-nav'),
+	contactButton = document.querySelector('#contact-nav');
+  scrollPosition = window.pageYOffset;
+
+//overcomplex way of toggling nav class//
+	window.onscroll = function (){
+		scrollPosition = window.scrollY;
+		console.log(scrollPosition);
+
+		if (scrollPosition <= 899) {
+			homeButton.classList.add("onPage");
+			teamButton.classList.remove("onPage");
+			workButton.classList.remove("onPage");
+			contactButton.classList.remove("onPage");
+		};
+		if (scrollPosition >= 900) {
+			homeButton.classList.remove("onPage");
+			teamButton.classList.add("onPage");
+			workButton.classList.remove("onPage");
+			contactButton.classList.remove("onPage");
+		};
+		if (scrollPosition >= 3254) {
+			teamButton.classList.remove("onPage");
+			workButton.classList.add("onPage");
+			contactButton.classList.remove("onPage");
+		};
+		if (scrollPosition >= 4967) {
+			workButton.classList.remove("onPage");
+			contactButton.classList.add("onPage");
+		};
+	};
+
+//Burger Menu Function//
+	function burgerMenu() {
+		burgerContent.classList.toggle("slideToggle");
+		navFiller.classList.toggle("fill-it");
+		burger.classList.toggle("expanded");
+	};
+
+	burger.addEventListener("click", burgerMenu, false);
 
 })();
