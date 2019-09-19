7c72ac02403f6933c4fb9ac5e28f09e5e52016ff
Jeffrey Helder <jeffrey.d.helder@gmail.com>
2019-09-19 00:13:43 -0400

    got card flip animation and bio working

--- js/main.js
+++ js/main.js
@@ -8,10 +8,16 @@
 	homeButton = document.querySelector('#home-nav'),
 	teamButton = document.querySelector('#team-nav'),
 	workButton = document.querySelector('#work-nav'),
-	contactButton = document.querySelector('#contact-nav');
+	contactButton = document.querySelector('#contact-nav'),
+	dilCard = document.querySelector('#Dil'),
+	yoonCard = document.querySelector('#Yoon'),
+	sujinCard = document.querySelector('#Sujin'),
+	jeffCard = document.querySelector('#Jeff'),
+	flipCard = document.querySelectorAll('.card'),
   scrollPosition = window.pageYOffset;
 
-//overcomplex way of toggling nav class//
+
+//my overcomplex way of toggling nav class//
 	window.onscroll = function (){
 		scrollPosition = window.scrollY;
 		console.log(scrollPosition);
@@ -39,6 +45,35 @@
 		};
 	};
 
+//Card Flip//
+
+//get id on mouseover//
+	flipCard.forEach(card =>{
+      card.onmouseover = function (){
+				profileCard = ("text/plain", this.id);
+				console.log(profileCard);
+//add class
+				document.querySelector(`#${profileCard}`).classList.add("flip");
+				document.querySelector(`#bio${profileCard}`).classList.remove("hidden");
+//remove class
+				 card.onmouseout = function (){
+					 document.querySelector(`#${profileCard}`).classList.remove("flip");
+					 document.querySelector(`#bio${profileCard}`).classList.add("hidden")
+				 };
+			};
+//"TOUCH" EVENT FOR MOBILE
+			card.onmousedown = function (e){
+				e.preventDefault();
+				profileCard = ("text/plain", this.id);
+				console.log(profileCard);
+//toggle class
+				document.querySelector(`#${profileCard}`).classList.toggle("flip");
+				document.querySelector(`#bio${profileCard}`).classList.toggle("hidden");
+			};
+
+			});
+
+
 //Burger Menu Function//
 	function burgerMenu() {
 		burgerContent.classList.toggle("slideToggle");
@@ -46,6 +81,7 @@
 		burger.classList.toggle("expanded");
 	};
 
+//Burger Menu Event
 	burger.addEventListener("click", burgerMenu, false);
 
 })();
