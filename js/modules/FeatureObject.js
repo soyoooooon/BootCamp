b66938d71513753ead85eb474575beecc80c7bee
Daffodil Ho <daffodilho@Daffodils-MBP.fanshawec.ca>
2019-09-20 21:02:47 -0400

    final commit

---
+++ js/modules/FeatureObject.js
@@ -0,0 +1,28 @@
+const Panel = {
+    Overview: {
+        image: "Overview.jpg",
+        heading: "Overview",
+        text: "The initial objective of PAPER PAPER is to create a social space for origami artists to not only share their work, but also to find useful resources as well as connect with other artists. In light of that, design and development of the site focused on creating a user-friendly and responsive website with easy-to-use functionalities and dynamic content."
+
+    },
+
+    Branding: {
+        image: "Branding.jpg",
+        heading: "Branding",
+        text: "Origami is a form of art that associates with values such as peacefulness, elegance, and creativity. With that in mind, branding of PAPER PAPER revolved around warm, fun and gentle color palettes and modern typography."
+    },
+
+    Feature: {
+        image: "Feature.jpg",
+        heading: "Feature",
+        text: "For maximum user experience, the finished website framework integrates: original imagery, animations and other media assets, blog posts and discussion threads, manageable content and media, e-commerce functionalities, a dynamic database, etc."
+    },
+
+    More: {
+        image: "MoreInfo.jpg",
+        heading: "More Info",
+        text: "Project started in January 2019. Technologies used include HTML5, CSS3, Javascript, PHP, and mySQL."
+    },
+}
+
+export default Panel;
